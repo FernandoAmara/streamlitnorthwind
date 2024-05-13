eddb6598437d3b3c4571b6b7e93d2e0d11d98c52
Fernando Amaral <93617541+FernandoAmara@users.noreply.github.com>
2024-05-13 13:08:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 
-# Função para conectar ao banco de dados SQLite e listar tabelas
+
 def list_tables():
     db_connection = sqlite3.connect('northwind.db')
     query = "SELECT name FROM sqlite_master WHERE type='table';"
@@ -10,7 +10,7 @@ def list_tables():
     db_connection.close()
     return tables['name'].tolist()
 
-# Função para listar campos de uma tabela específica
+
 def list_table_columns(table_name):
     db_connection = sqlite3.connect('northwind.db')
     query = f"PRAGMA table_info({table_name});"
@@ -18,21 +18,21 @@ def list_table_columns(table_name):
     db_connection.close()
     return columns['name'].tolist()
 
-# Função para conectar ao banco de dados SQLite
+
 def run_query(query):
     db_connection = sqlite3.connect('northwind.db')
     query_result = pd.read_sql_query(query, db_connection)
     db_connection.close()
     return query_result
 
-# Configuração para utilizar toda a largura da página
+
 st.set_page_config(layout="wide")
 
-# Interface do Streamlit
+
 def main():
     st.title('Executar consulta banco Northwind')
 
-    # Sidebar com lista de tabelas e campos
+
     st.sidebar.title("Tabelas e Campos do Banco de Dados")
     tables = list_tables()
     for table in tables:
@@ -41,14 +41,14 @@ def main():
             for column in columns:
                 st.sidebar.text(f"   - {column}")
 
-    # Caixa de texto para a entrada da consulta SQL
+
     query = st.text_area("Insira sua consulta SQL aqui:", height=150)
 
-    # Botão para executar a consulta
+
     if st.button('Executar'):
         if query:
             try:
-                # Executa a consulta e mostra o resultado
+
                 result = run_query(query)
                 st.dataframe(result, use_container_width=True)
             except Exception as e:
